354962429c59bbe9c3aae413909a3061f6f8d722
Tam Pham <tampham.dev@gmail.com>
2022-03-15 13:41:57 +0700

    Add paginate with react query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+
 const queryClient = new QueryClient();
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Star Wars Info</h1>
-        <Navbar setPage={setPage} />
-        <div className="content">
-          {page === "planets" ? <Planets /> : <People />}
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Star Wars Info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === "planets" ? <Planets /> : <People />}
+          </div>
         </div>
-      </div>
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }
 
